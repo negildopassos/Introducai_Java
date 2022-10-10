555c8d8a3a95b8887fdace7d6cac43c00174bb61
Negildo Passos <negildojr@gmail.com>
2022-10-10 20:48:07 -0300

    Variaveis

---
+++ src/com/negildo/introducaojava/Variaveis.java
@@ -0,0 +1,10 @@
+package com.negildo.introducaojava;
+
+public class Variaveis {
+
+    public static void main(String[] args) {
+        int idade;
+        int idade1 = 20;
+        String maiorAltura;
+    }
+}
