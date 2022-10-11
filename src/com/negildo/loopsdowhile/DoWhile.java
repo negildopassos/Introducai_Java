8d59b550f66161440647abc976a75e7e537a19fb
Negildo Passos <negildojr@gmail.com>
2022-10-10 22:23:35 -0300

    Loops While e Do while

---
+++ src/com/negildo/loopsdowhile/DoWhile.java
@@ -0,0 +1,17 @@
+package com.negildo.loopsdowhile;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 1;
+        int max = 10;
+
+
+        do {
+            i++;
+            System.out.println("Valor de i :"  +i);
+
+        }while (i<15);
+
+        System.out.println(i);
+    }
+}
---
+++ src/com/negildo/loopwhile/LoopWhile.java
@@ -0,0 +1,15 @@
+package com.negildo.loopwhile;
+
+public class LoopWhile {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int max = 10;
+
+        while (i < max){
+            System.out.println("Valor de i: " + i);
+            i++;
+        }
+
+    }
+}
