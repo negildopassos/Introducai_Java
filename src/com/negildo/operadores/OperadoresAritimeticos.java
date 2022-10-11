183a81d7f0fd4fa36b0b8b9eab9f83b865675583
Negildo Passos <negildojr@gmail.com>
2022-10-10 21:38:26 -0300

    Operadores

---
+++ src/com/negildo/operadores/OperadoresAritimeticos.java
@@ -0,0 +1,50 @@
+package com.negildo.operadores;
+
+public class OperadoresAritimeticos {
+    public static void main(String[] args) {
+         int resultado = 1+2;
+
+         System.out.println(resultado);
+
+         resultado = resultado - 1;
+        System.out.println(resultado);
+
+        resultado = resultado * 4;
+         System.out.println(resultado);
+
+        resultado = resultado / 2;
+        System.out.println(resultado);
+
+        resultado = resultado + 2;
+        System.out.println(resultado);
+
+        resultado = resultado % 5;
+        System.out.println(resultado);
+
+
+
+        resultado = resultado + 1;
+        System.out.println(resultado);
+
+        resultado++;
+        System.out.println(resultado);
+
+
+        System.out.println(resultado++);
+
+        System.out.println(++resultado);
+
+
+
+
+        resultado--;
+        System.out.println(resultado);
+
+
+        System.out.println(resultado--);
+
+        System.out.println(--resultado);
+
+
+    }
+}
---
+++ src/com/negildo/operadores/OperadoresRelacionais.java
@@ -0,0 +1,18 @@
+package com.negildo.operadores;
+
+public class OperadoresRelacionais {
+
+    public static void main(String[] args) {
+        int valor1 = 1;
+        int valor2 = 2;
+
+
+
+        System.out.println("Valor 1 == valor2: " + (valor1==valor2));
+        System.out.println("Valor 1 != valor2: " + (valor1!=valor2));
+        System.out.println("Valor 1 > valor2: " + (valor1>valor2));
+        System.out.println("Valor 1 < valor2: " + (valor1<valor2));
+        System.out.println("Valor 1 <= valor2: " + (valor1<=valor2));
+        System.out.println("Valor 1 >= valor2: " + (valor1>=valor2));
+    }
+}
